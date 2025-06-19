44d7a08008c796b9408f4ffa989e86975d7a4daf
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2025-06-19 11:17:11 +0530

    Add files via upload

---
+++ Battery/BatteryDriver.java
@@ -0,0 +1,10 @@
+package Battery;
+
+public class BatteryDriver {
+    public static void main(String[] args) {
+        Battery b1 = new Battery("Duracell", 150.0, 3000, "Li-ion");
+        b1.displayInfo();
+
+    }
+    
+}
